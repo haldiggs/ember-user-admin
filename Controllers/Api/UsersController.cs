ca1a272bb741a90a3a85940a173d1f1c20f5c65a
Rob Conery <rob@wekeroad.com>
2014-03-20 14:51:59 -1000

    initial

---
+++ Controllers/Api/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web.Models;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Web.Controllers
+{
+
+    [Authorize(Roles="admin")]  
+    public class UsersController : ApiController
+    {
+        
+        // GET api/users
+        public dynamic Get()
+        {
+            var db = new ApplicationDbContext();
+            var users = db.Users;
+            var result = new List<dynamic>();
+            var roles = new List<dynamic>();
+            var indexer = 1;
+            foreach (var user in users)
+            {
+                result.Add(new
+                {
+                    id = indexer,
+                    userName = user.UserName,
+                    email = user.Email,
+                    first = user.First,
+                    last = user.Last,
+                    bio = user.Bio,
+                    twitter = user.Twitter,
+                    roles = user.Roles.Select(x => int.Parse(x.RoleId)).ToArray()
+                });
+                indexer++;
+
+            }
+            foreach (var role in db.Roles)
+            {
+                roles.Add(new { id = int.Parse(role.Id), name = role.Name });
+            }
+            var output = new {users = result, roles = roles};
+            return output;
+
+        }
+
+        // GET api/users/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/IdentityModels.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+namespace Web.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class ApplicationUser : IdentityUser
+    {
+        public string Email { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Bio { get; set; }
+        public string Twitter { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
